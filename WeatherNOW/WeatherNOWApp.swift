86840fe6de83289fdb0aff8911bb11e7c125ae63
Dhaval Sharma <dhavalsharma1724@gmail.com>
2021-09-10 15:58:27 -0400

    Initial Commit

---
+++ WeatherNOW/WeatherNOWApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherNOWApp.swift
+//  WeatherNOW
+//
+//  Created by Dhaval Sharma on 9/10/21.
+//
+
+import SwiftUI
+
+@main
+struct WeatherNOWApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
