7253a382729b16fb920506f885fdbe2a0400a1fd
DhavalSharma1724 <dhavalsharma1724@gmail.com>
2021-09-16 18:51:59 -0400

    - Add description for detail card view

--- WeatherNOW/Views/CardDetailView.swift
+++ WeatherNOW/Views/CardDetailView.swift
@@ -13,12 +13,35 @@ struct CardDetailView: View {
     var gradient: Gradient
     var value: Double = 0
     var unit: String = ""
+    var description: String = ""
+    var rectangleHeight = 1
     
     init(topic: String, gradient: Gradient, value: Double, unit: String){
         self.topic = topic
         self.gradient = gradient
         self.value = value
         self.unit = unit
+        
+        switch topic{
+        case "Humidity":
+            self.description = ""
+            rectangleHeight = 241
+            break
+        case "Dew Point":
+            self.description = ""
+            break
+        case "UV Index":
+            self.description = ""
+            break
+        case "Wind Speed":
+            self.description = ""
+            break
+        case "Clouds":
+            self.description = ""
+            break
+        default:
+            break
+        }
         
     }
     
@@ -31,6 +54,7 @@ struct CardDetailView: View {
                 Text(topic)
                     .font(Font.custom("Avenir Heavy", size: 48))
                     .foregroundColor(.white)
+                    .padding(.top, 50)
                 
                 HStack(alignment: .bottom, spacing: -4){
                     Text(topic == "Wind Speed" ? String(Int(value)) : String(Int(value)) + unit)
@@ -39,7 +63,38 @@ struct CardDetailView: View {
                         .font(Font.custom("Avenir Heavy", size: 60))
                         .padding(.bottom, 18)
                 }
+                .padding(.bottom, -4)
+                ZStack{
+                    //Background
+                    Rectangle()
+                        .foregroundColor(.black)
+                        .opacity(0.05)
+                        .cornerRadius(10)
+                    
+                    VStack (alignment: .leading){
+                        Text("What does this mean?")
+                            .font(Font.custom("Avenir Heavy", size: 24))
+                            .padding(.vertical, -5)
+                            .padding(.top, 25)
+                        
+                        //Divider
+                        Rectangle()
+                            .opacity(0.8)
+                            .cornerRadius(20)
+                            .frame(width: 100, height: 3)
+                            .foregroundColor(.white)
+                        
+                        Text("Humidity measures the amount of water vapor in the air. In other words, if the humidity is at 100%, the air cannot hold any more water vapor. This means sweat cannot evaporate off your body, making it feel hotter than it actually is. If the humidity is low, sweat easily evaporates, making you feel colder.")
+                            .font(Font.custom("Avenir", size: 16))
+                        
+                        Spacer()
+                    }
+                    .padding(.horizontal)
+                }
+                .padding(.horizontal, 25)
+                .frame(height: CGFloat(rectangleHeight))
                 
+                Spacer()
             }
             
             
