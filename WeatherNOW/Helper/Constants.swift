529704314f88edd865342463b83dd9356c1fc18e
DhavalSharma1724 <dhavalsharma1724@gmail.com>
2021-09-21 17:27:20 -0400

    - Commit Constants

---
+++ WeatherNOW/Helper/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  WeatherNOW
+//
+//  Created by Dhaval Sharma on 9/21/21.
+//
+
+import Foundation
+
+struct Constants {
+    
+    static var apiKey = "7ef1538a1ddb3108adab0227bde60492"
+    static var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?"
+    //curl --request GET --url \
+}
